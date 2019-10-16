ac07845ab63434812f6b8a6070a5a618e4050e25
phu-pmh <phumon91@gmail.com>
2019-10-15 23:20:05 -0400

    SQuAD

---
+++ jiant/tasks/tet_squad.py
@@ -0,0 +1,73 @@
+import json
+
+def is_whitespace(c):
+    if c == " " or c == "\t" or c == "\r" or c == "\n" or ord(c) == 0x202F:
+        return True
+    return False
+ 
+def load():
+    is_training=True
+    version_2_with_negative=True 
+    path="/scratch/pmh330/jiant-data/SQuAD/train-v2.0.json"
+    with open(path) as f:
+        input_data = json.load(f)["data"]
+    examples = []
+    for entry in input_data:
+        for paragraph in entry["paragraphs"]:
+            paragraph_text = paragraph["context"]
+            doc_tokens = []
+            char_to_word_offset = []
+            prev_is_whitespace = True
+            for c in paragraph_text:
+                if is_whitespace(c):
+                    prev_is_whitespace = True
+                else:
+                    if prev_is_whitespace:
+                        doc_tokens.append(c)
+                    else:
+                        doc_tokens[-1] += c
+                    prev_is_whitespace = False
+                char_to_word_offset.append(len(doc_tokens) - 1)
+
+            print("char_to_word:",char_to_word_offset)
+
+            for qa in paragraph["qas"]:
+                qas_id = qa["id"]
+                question_text = qa["question"]
+                start_position = None
+                end_position = None
+                orig_answer_text = None
+                is_impossible = False
+                if is_training:
+                    if version_2_with_negative:
+                        is_impossible = qa["is_impossible"]
+                    if (len(qa["answers"]) != 1) and (not is_impossible):
+                        raise ValueError(
+                            "For training, each question should have exactly 1 answer.")
+                    if not is_impossible:
+                        answer = qa["answers"][0]
+                        orig_answer_text = answer["text"]
+                        answer_offset = answer["answer_start"]
+                        answer_length = len(orig_answer_text)
+                        start_position = char_to_word_offset[answer_offset]
+                        end_position = char_to_word_offset[answer_offset + answer_length - 1]
+                        print("answer: ", orig_answer_text)
+                        print("answer_offset: ", answer_offset)
+                        print("start_position: ", start_position)
+                        print("end: ", end_position)
+                        print("\n")
+                        # Only add answers where the text can be exactly recovered from the
+                        # document. If this CAN'T happen it's likely due to weird Unicode
+                        # stuff so we will just skip the example.
+                        #
+                        # Note that this means for training mode, every example is NOT
+                        # guaranteed to be preserved.
+                        actual_text = " ".join(doc_tokens[start_position:(end_position + 1)])
+                    else:
+                        start_position = -1
+                        end_position = -1
+                        orig_answer_text = ""
+            
+            sys.exit(0)
+
+load()
